0d26dea6d36d5609ccf432dfa516056c328dd97c
Cillah <102717756+Cillah@users.noreply.github.com>
2023-01-23 15:46:23 +0530

    secret key update

--- landing_page/settings.py
+++ landing_page/settings.py
@@ -12,8 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
-
+import os
 load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
