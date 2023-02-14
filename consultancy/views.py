4d756ef32cc17188abc12b5b78202f4d1ec8971c
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-02-14 21:07:43 +0530

    enabled post method for emailing

--- consultancy/views.py
+++ consultancy/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
     return render(request,'consultancy/index.html')
+
+def contact(request):
+    print(request.POST)
+    return redirect("home")
