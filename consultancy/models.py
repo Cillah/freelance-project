725400af9c38680f37585371f3833d858ecddd7a
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-02-20 19:42:43 +0530

    added uuid to models

--- consultancy/models.py
+++ consultancy/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class Training(models.Model):
+  id = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
   training_topic = models.CharField(max_length=200)
   training_poster = models.ImageField(max_length=200,null=True,upload_to="training_poster")
   speaker = models.CharField(max_length=200)
   speaker_image = models.ImageField(max_length=200,null=True,upload_to="training_speaker")
   date = models.DateField()
   timings = models.CharField(max_length=200)
-  remarks = models.CharField(max_length=200,null=True)
+  remarks = models.CharField(max_length=1000,null=True)
 
   def __str__(self) -> str:
     #returns the training topic name as object.
