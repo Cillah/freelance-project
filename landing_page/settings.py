54fa6a61ea52e9439c50c1e7faad145340b13668
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-03-06 09:51:38 +0530

    updated secret variables to avoid os conflict

--- landing_page/settings.py
+++ landing_page/settings.py
@@ -89,11 +89,11 @@ WSGI_APPLICATION = 'landing_page.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('NAME'),
+        'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
-        'HOST': os.getenv('HOST'),
-        'PORT': os.getenv('PORT')
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
