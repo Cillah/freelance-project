446ae4f924bdebf2cd1cf745ba12b2b8cf5944d1
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-03-13 21:03:02 +0530

    varghese typo fixed

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 
-admin.site.site_header = 'Philip Varghese Consultancies'
+admin.site.site_header = 'Philip Varughese Consultancies'
 #Add the below line
 admin.site.index_title = 'Trainings App'
 
